939ba9c86564c32952b815cf0d10b628f14a8334
Aminur-Rahman-Kanon <kanon754@gmail.com>
2024-10-18 23:12:44 +0100

    update receiver email

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 app.use(bodyParser.json());
-app.use(cors({ origin: ['http://localhost:3000', 'https://new-era-glazing-solution-uze8.onrender.com'] }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://new-era-glazing-solution-uze8.onrender.com', 'https://neweraglazingsolutionsltd.co.uk'] }));
 const requestAssetsDownload = require('./routes/requestAssetsDownload');
 const requestAssetsPost = require('./routes/requestAssetPost');
 const fetchProduct = require('./routes/fetchProduct');
